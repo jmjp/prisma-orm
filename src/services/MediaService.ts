3fc56c2331adbddb9176e6adef2ea3a0a2484745
João Miguel <joaozinhosleepy@gmail.com>
2022-06-03 21:48:45 -0300

    update

--- src/services/MediaService.ts
+++ src/services/MediaService.ts
@@ -1,9 +1,9 @@
 import ImageKit from 'imagekit';
 
 const MediaService = new ImageKit({
-    publicKey : "public_xorM/0JjjB6U3V6txj+3Q8hiciE=",
-    privateKey : "private_JayXMQv5MKOkN2UKLYuRYa5JphY=",
-    urlEndpoint : "https://ik.imagekit.io/jmgsoftwares"
+    publicKey : "",
+    privateKey : "",
+    urlEndpoint : ""
 })
 
 export { MediaService}
